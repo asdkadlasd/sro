4a52a52c8d36a5563c13eca9208381f35b19024a
asdkadlasd <cuentabtb@gmail.com>
2022-10-25 14:32:36 -0600

    Scripts

--- scripts/scripts/00-laravel-deploy.sh
+++ scripts/scripts/00-laravel-deploy.sh
@@ -3,11 +3,17 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "NPM install..."
+npm install
+
+echo "NPM run dev..."
+npm run dev
+
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+echo "Running migrations seeders..."
+php artisan migrate:fresh --seed
