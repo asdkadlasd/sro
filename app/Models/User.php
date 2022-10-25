3cdfe021abc853d1e3db8f9c34f4c61e278b3587
asdkadlasd <cuentabtb@gmail.com>
2022-10-25 13:44:34 -0600

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class User extends Authenticatable implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile',
+        'phone',
+        'status',
+        'image'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getImagenAttribute($image)
+    {
+        if ($this->image != NULL) {
+            return (file_exists('storage/users/' . $this->image) ? $this->image : 'userimg.jpg');
+        } else {
+            return 'userimg.jpg';
+        }
+    }
+}
